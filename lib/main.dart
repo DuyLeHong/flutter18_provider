e256bf23c7aa636c3c3618b1b38eb78a947f3d30
Duy Le <lehongduy.bk@gmail.com>
2022-08-30 20:30:23 +0700

    multi provider example

--- lib/main.dart
+++ lib/main.dart
@@ -10,65 +10,83 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     var myData = MyModel();
 
-    // return Provider<MyModel>(
-    //   create: (context) => myData,
-    //   child: (),
-    // );
-
-    return ChangeNotifierProvider<MyModel>(
-      create: (BuildContext context) {
-        return MyModel();
-      },
-      child: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(title: Text('My App')),
-          backgroundColor: Colors.grey,
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(20),
-                color: Colors.green[200],
-                child: Builder(
-                  builder: (BuildContext context) {
-                    return ElevatedButton(
-                      onPressed: () {
-                        //myData.doSomething();
-
-                        MyModel data = Provider.of<MyModel>(context, listen: false);
-                        data.doSomething();
-                      },
-                      child: Text('Click me!'),
-                    );
-                  },
-                ),
-              ),
-              // child: Consumer<MyModel>(
-              //   builder: (context, mymodel, child) {
-              //     return RaisedButton(
-              //       child: Text('Do something'),
-              //       onPressed: () {
-              //         mymodel.doSomething();
-              //       },
-              //     );
-              //   },
-              // ),
-              Container(
-                padding: const EdgeInsets.all(35),
-                color: Colors.blue[200],
-                child: Consumer<MyModel>(
-                  builder: (context, mymodel, child) {
-                    return Text(mymodel.text);
-                  },
-                ),
-              ),
-            ],
-          ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<MyModel>(
+          create: (BuildContext context) {
+            return MyModel();
+          },
         ),
+
+        Provider<String>(
+          create: (ctx) {
+            return "CodeFresher - lesson 9";
+          },
+        ),
+
+      ],
+      child: MaterialApp(
+        home: _buildHomeScreen(),
       ),
     );
+
+    // return Provider<String>(
+    //   create: (ctx) {
+    //     return "CodeFresher - lesson 9";
+    //   },
+    //   child: ChangeNotifierProvider<MyModel>(
+    //     create: (BuildContext context) {
+    //       return MyModel();
+    //     },
+    //     child: MaterialApp(
+    //       home: _buildHomeScreen(),
+    //     ),
+    //   ),
+    // );
   }
+}
+
+_buildHomeScreen() {
+  return Scaffold(
+    appBar: AppBar(title: Text('My App')),
+    backgroundColor: Colors.grey,
+    body: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(20),
+          color: Colors.green[200],
+          child: Builder(
+            builder: (BuildContext context) {
+              return ElevatedButton(
+                onPressed: () {
+                  //myData.doSomething();
+
+                  MyModel data = Provider.of<MyModel>(context, listen: false);
+                  data.doSomething();
+
+                  String sData = Provider.of<String>(context, listen: false);
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text(sData)));
+                },
+                child: Text('Click me!'),
+              );
+            },
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(35),
+          color: Colors.blue[200],
+          child: Consumer<MyModel>(
+            builder: (context, mymodel, child) {
+              return Text(mymodel.text);
+            },
+          ),
+        ),
+      ],
+    ),
+  );
 }
 
 class MyModel extends ChangeNotifier {
